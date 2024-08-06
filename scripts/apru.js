885c64ec51325af36948c213280f86d19cf83517
ilovetyfng <81743178+depressed-cubic@users.noreply.github.com>
2024-08-06 14:10:08 +0800

    improved layout

--- scripts/apru.js
+++ scripts/apru.js
@@ -8,7 +8,13 @@ async function main() {
 
     let nameMap = new Object()
 
-    // return an array of ust equivalent of the course, consider said uni through virtual study as well, null if not found
+    /**
+     * return an array of ust equivalent of the course, consider said uni through virtual study as well, null if not found
+     * @param {string} uni name of the university without weird stuff like (Academic) 
+     * @param {string} code source course code
+     * @param {string} name source course name
+     * @returns array of object with property `code`, `name`, `credits` (can be empty)
+     */
     function course_mapper(uni, code, name) {
         /*
         if (data[uni + " through virtual study"][code] == null) {
@@ -99,8 +105,9 @@ async function main() {
         return nameMap[uni][name]
 
     }
-
+/*
     function insertContent(row, mapped_courses) {
+       /* 
         row.children[0].textContent = row.children[0].textContent + " " + row.children[1].textContent
 
         row.children[1].textContent = "=>"
@@ -113,6 +120,85 @@ async function main() {
                 row.appendChild(document.createElement("td")).textContent = course["code"] + " " + course["name"] + ((course["virtual"]) ? "" : " (not via virtual)")
             }
         }
+        const hostCourse = new Object()
+        hostCourse["code"] = row.children[0].textContent
+        hostCourse["name"] = row.children[1].textContent
+
+        const kek = new MappingDisplay(hostCourse, mapped_courses)
+    }*/
+
+    class MappingTile{
+        source;
+        mapsto = [];
+        mainDiv;
+
+        /**
+         * 
+         * @param {{code: string, name: string}} source 
+         * @param {{code: string, name: string, credits: string}[]} mapped_courses 
+         */
+        constructor (source, mapped_courses) {
+            this.mainDiv = document.createElement("div")
+            this.mainDiv.classList.add("course-mapping")
+            this.source = source
+            this.mapsto = mapped_courses
+
+            this.update()
+        }
+
+        #courseTile(className, showCredits = false) {
+            const div = document.createElement("div")
+            div.classList.add(className + "-course-tile")
+            div.appendChild(document.createElement("div"))
+            div.appendChild(document.createElement("div"))
+            div.children[0].classList.add(className + "-course-code")
+            div.children[1].classList.add(className + "-course-name")
+            if (showCredits) {
+                div.appendChild(document.createElement("div"))
+                div.children[2].classList.add(className + "-course-credits")
+            }
+            return div
+        }
+
+        update() {
+            this.mainDiv.appendChild(this.#courseTile("host"))
+
+            const hostCourseCode = this.mainDiv.children[0].children[0]
+            hostCourseCode.textContent = this.source.code
+            
+            const hostCourseName = this.mainDiv.children[0].children[1]
+            hostCourseName.textContent = this.source.name
+            
+            this.mainDiv.appendChild(document.createElement("div"))
+            this.mainDiv.children[1].classList.add("mapped-courses")
+        
+            if (this.mapsto == undefined) {
+                const div = document.createElement("div")
+                div.textContent = "no existing mapping"
+                this.mainDiv.appendChild(div)
+                return
+            }
+            this.mapsto.forEach(c => {
+
+                const mappedCourseDiv = this.#courseTile("mapped", true)
+                const arrow = document.createElement("div")
+                arrow.classList.add("arrow")
+                arrow.textContent = "=>"
+                mappedCourseDiv.children[0].before(arrow)
+
+                const code = mappedCourseDiv.children[1]
+                code.textContent = c.code
+
+                const name = mappedCourseDiv.children[2]
+                name.textContent = c.name
+
+                const credits = mappedCourseDiv.children[3]
+                credits.textContent = c.credits
+                
+                this.mainDiv.children[1].appendChild(mappedCourseDiv)
+            })
+        }
+
     }
 
     function boxHandler(element) {
@@ -134,16 +220,23 @@ async function main() {
             // Make sure there are courses
             if (courses != null) {
                 let rows = [...courses.querySelectorAll("tr")]
+
+                const newContainer = document.createElement("div")
             
                 rows.forEach((row) => {
-                    
-                    const hostCode = row.children[0].textContent
-                    const hostName = row.children[1].textContent
+                    const hostCourse = new Object()
+                    hostCourse["code"] = row.children[0].textContent
+                    hostCourse["name"] = row.children[1].textContent
 
-                    const ustCourses = course_mapper(uni, hostCode, hostName)
+                    const ustCourses = course_mapper(uni, hostCourse["code"], hostCourse["name"])
 
-                    insertContent(row, ustCourses)
+                    const kek = new MappingTile(hostCourse, ustCourses)
+                    newContainer.appendChild(kek.mainDiv)
                 })
+
+                courses.after(newContainer)
+
+                courses.remove()
             }
         }
     }
